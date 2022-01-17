a236a97001837c9afa6fe829c38395290f92c5a0
Jacob Fernandes <59456400+proballstar@users.noreply.github.com>
2022-01-16 17:59:34 -0800

    Transfer to Desktop RF 1

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module  } from '@nestjs/common';
 import { ReportsService } from './service/reports.service';
 import { ReportsController } from './controller/reports.controller';
 
